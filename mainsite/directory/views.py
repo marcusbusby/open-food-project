c28b865411d8f592691f100d3409a8e263ea22d3
Marcus Busby <marcusdbusby@gmail.com>
2015-07-13 16:53:38 +0200

    model to template functionality

--- mainsite/directory/views.py
+++ mainsite/directory/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Food
 
+def food_list(request):
+	foods = Food.objects.all()
+	return render(request, 'directory/food_list.html', {'foods': foods})
 # Create your views here.
